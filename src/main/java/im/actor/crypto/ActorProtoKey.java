19e0352290de923a41632606a615ef910c3a0758
Steve Kite <steve@actor.im>
2016-01-08 22:58:30 +0300

    fix(proto): Fixing proto keys

--- src/main/java/im/actor/crypto/ActorProtoKey.java
+++ src/main/java/im/actor/crypto/ActorProtoKey.java
@@ -23,9 +23,7 @@ public class ActorProtoKey {
         serverMacKey = ByteStrings.substring(masterKey, (offset++) * 32, 32);
         clientKey = ByteStrings.substring(masterKey, (offset++) * 32, 32);
         serverKey = ByteStrings.substring(masterKey, (offset++) * 32, 32);
-
-        // Moving to russian part
-        offset = 128;
+        
         clientMacRussianKey = ByteStrings.substring(masterKey, (offset++) * 32, 32);
         serverMacRussianKey = ByteStrings.substring(masterKey, (offset++) * 32, 32);
         clientRussianKey = ByteStrings.substring(masterKey, (offset++) * 32, 32);
